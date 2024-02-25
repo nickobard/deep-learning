1a6d01eac6cef67c49f65cd0284820d8cb9b4d53
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-02-25 13:28:51 +0100

    Finish numpy entropy task

--- numpy_entropy/numpy_entropy.py
+++ numpy_entropy/numpy_entropy.py
@@ -29,33 +29,50 @@ def main(args: argparse.Namespace) -> tuple[float, float, float]:
 
     outcomes_number = sum(frequencies.values())
     probabilities = {outcome: frequency / outcomes_number for outcome, frequency in frequencies.items()}
-    print(probabilities)
 
     # TODO: Create a NumPy array containing the data distribution. The
     # NumPy array should contain only data, not any mapping. Alternatively,
     # the NumPy array might be created after loading the model distribution.
 
+    data_distribution = np.array([probabilities[outcome] for outcome in sorted(probabilities)])
+
     # TODO: Load model distribution, each line `string \t probability`.
+
+    model_distribution_mapping = {}
+
     with open(args.model_path, "r") as model:
         for line in model:
             line = line.rstrip("\n")
             # TODO: Process the line, aggregating using Python data structures.
 
+            data_element, probability = line.split()
+            model_distribution_mapping[data_element] = float(probability)
+
     # TODO: Create a NumPy array containing the model distribution.
+    model_distribution = np.array(
+        [model_distribution_mapping[outcome] for outcome in sorted(model_distribution_mapping)])
 
     # TODO: Compute the entropy H(data distribution). You should not use
     # manual for/while cycles, but instead use the fact that most NumPy methods
     # operate on all elements (for example `*` is vector element-wise multiplication).
-    entropy = ...
+    entropy = -(sum(data_distribution * np.log(data_distribution)))
 
     # TODO: Compute cross-entropy H(data distribution, model distribution).
     # When some data distribution elements are missing in the model distribution,
     # return `np.inf`.
-    crossentropy = ...
+
+    if frequencies.keys() == model_distribution_mapping.keys():
+        crossentropy = -sum(data_distribution * np.log(model_distribution))
+    else:
+        crossentropy = np.inf
 
     # TODO: Compute KL-divergence D_KL(data distribution, model_distribution),
     # again using `np.inf` when needed.
-    kl_divergence = ...
+
+    if frequencies.keys() == model_distribution_mapping.keys():
+        kl_divergence = sum(data_distribution * (np.log(data_distribution) - np.log(model_distribution)))
+    else:
+        kl_divergence = np.inf
 
     # Return the computed values for ReCodEx to validate.
     return entropy, crossentropy, kl_divergence
