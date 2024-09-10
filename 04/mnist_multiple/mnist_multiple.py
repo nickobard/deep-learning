8776a599fdfef050fab6efe738d4b65f4ced1dc1
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-09-10 20:22:41 +0200

    04_mm: Implementation with manually split tensors. With error.

--- 04/mnist_multiple/mnist_multiple.py
+++ 04/mnist_multiple/mnist_multiple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+
 os.environ.setdefault("KERAS_BACKEND", "torch")  # Use PyTorch backend unless specified otherwise
 
 import numpy as np
@@ -16,6 +17,8 @@ parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
 parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 
@@ -35,6 +38,21 @@ class Model(keras.Model):
         # - flattening layer,
         # - fully connected layer with 200 neurons and ReLU activation,
         # obtaining a 200-dimensional feature vector FV of each image.
+        rescaled = keras.layers.Rescaling(1 / 255)(images)
+        conv1 = keras.layers.Conv2D(filters=10, kernel_size=(3, 3), strides=(2, 2), padding='valid',
+                                    activation='relu')
+        hidden_1 = conv1(rescaled[0])
+        hidden_2 = conv1(rescaled[1])
+        conv2 = keras.layers.Conv2D(filters=20, kernel_size=(3, 3), strides=(2, 2), padding='valid',
+                                    activation='relu')
+        hidden_1 = conv2(hidden_1)
+        hidden_2 = conv2(hidden_2)
+        flatten_layer = keras.layers.Flatten()
+        hidden_1 = flatten_layer(hidden_1)
+        hidden_2 = flatten_layer(hidden_2)
+        d1 = keras.layers.Dense(units=200, activation='relu')
+        hidden_1 = d1(hidden_1)
+        hidden_2 = d1(hidden_2)
 
         # TODO: Using the computed representations, the model should produce four outputs:
         # - first, compute _direct comparison_ whether the first digit is
@@ -49,11 +67,25 @@ class Model(keras.Model):
         # - finally, compute _indirect comparison_ whether the first digit
         #   is greater than second, by comparing the predictions from the above
         #   two outputs; convert the comparison to "float32" using `keras.ops.cast`.
+
+        direct_hidden = keras.layers.Concatenate(axis=0)([hidden_1, hidden_2])
+        direct_hidden = keras.layers.Dense(units=200, activation='relu')(direct_hidden)
+        direct_output = keras.layers.Dense(units=1, activation='sigmoid')(direct_hidden)
+
+        softmax = keras.layers.Dense(units=MNIST.LABELS, activation='softmax')
+        indirect_output_1 = softmax(hidden_1)
+        indirect_output_2 = softmax(hidden_2)
+
+        digit_1 = keras.ops.argmax(indirect_output_1)
+        digit_2 = keras.ops.argmax(indirect_output_2)
+
+
+
         outputs = {
-            "direct_comparison": ...,
-            "digit_1": ...,
-            "digit_2": ...,
-            "indirect_comparison": ...,
+            "direct_comparison": direct_output,
+            "digit_1": indirect_output_1,
+            "digit_2": indirect_output_2,
+            "indirect_comparison": digit_1 > digit_2,
         }
 
         # Finally, construct the model.
@@ -73,19 +105,19 @@ class Model(keras.Model):
         self.compile(
             optimizer=keras.optimizers.Adam(),
             loss={
-                "direct_comparison": ...,
-                "digit_1": ...,
-                "digit_2": ...,
+                "direct_comparison": keras.losses.BinaryCrossentropy(),
+                "digit_1": keras.losses.CategoricalCrossentropy(),
+                "digit_2": keras.losses.CategoricalCrossentropy(),
             },
             metrics={
-                "direct_comparison": [...],
-                "indirect_comparison": [...],
+                "direct_comparison": [keras.metrics.BinaryAccuracy(name='accuracy')],
+                "indirect_comparison": [keras.metrics.BinaryAccuracy(name='accuracy')],
             },
         )
 
     # Create an appropriate dataset using the MNIST data.
     def create_dataset(
-        self, mnist_dataset: MNIST.Dataset, args: argparse.Namespace,
+            self, mnist_dataset: MNIST.Dataset, args: argparse.Namespace,
     ) -> torch.utils.data.Dataset:
         # Original MNIST dataset.
         images, labels = mnist_dataset.data["images"], mnist_dataset.data["labels"]
@@ -95,7 +127,7 @@ class Model(keras.Model):
         class TorchDataset(torch.utils.data.Dataset):
             def __len__(self) -> int:
                 # TODO: The new dataset has half the size of the original one.
-                return ...
+                return int(len(images) / 2)
 
             def __getitem__(self, index: int) -> tuple[tuple[np.ndarray, np.ndarray], dict[str, np.ndarray]]:
                 # TODO: Given an `index`, generate a dataset element suitable for our model.
@@ -103,7 +135,7 @@ class Model(keras.Model):
                 # - `input` being a pair of images `(images[2 * index], images[2 * index + 1])`,
                 # - `output` being a dictionary with keys "digit_1", "digit_2", "direct_comparison",
                 #   and "indirect_comparison".
-                return ...
+                return (images[2 * index], images[2 * index + 1]), (labels[2 * index], labels[2 * index + 1])
 
         return TorchDataset()
 
